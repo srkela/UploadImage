1a5ac7738a9215a1469efa6fab8176c20d2c07ed
Srdjan Djenader <djenka76@gmail.com>
2021-08-22 15:18:33 -0500

    adjusted App.js for redux

--- app/App.js
+++ app/App.js
@@ -6,17 +6,19 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React from "react";
 // eslint-disable-next-line no-duplicate-imports
-import type {Node} from 'react';
+import type { Node } from "react";
+import { Provider } from "react-redux";
 
-import Landing from './components/screens/Landing';
-
+import UploadImage from "./components/screens/UploadImage";
+import store from "./redux/store";
 
 const App: () => Node = () => {
-
   return (
-      <Landing/>
+    <Provider store={store}>
+      <UploadImage />
+    </Provider>
   );
 };
 
