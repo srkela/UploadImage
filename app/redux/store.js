e3b4336352ebc0a2b52ebe783b25ddad26f4611a
Srdjan Djenader <djenka76@gmail.com>
2021-08-22 14:40:49 -0500

    created store.js

---
+++ app/redux/store.js
@@ -0,0 +1,39 @@
+import * as Platform from "react-native";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import reducer from "./reducers";
+
+let composeEnhancers = compose;
+// eslint-disable-next-line no-undef
+if (__DEV__) {
+  // Use it if Remote debugging with RNDebugger, otherwise use remote-redux-devtools
+  /* eslint-disable no-underscore-dangle */
+  composeEnhancers = (
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || require("remote-redux-devtools").composeWithDevTools
+  )({
+    name: Platform.OS,
+    ...require("../../package.json").remotedev,
+  });
+  /* eslint-enable no-underscore-dangle */
+}
+
+const middleware = [thunk, standaloneLogger];
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+function configureStore(initialState) {
+  const store = createStore(reducer, initialState, enhancer);
+  if (module.hot) {
+    module.hot.accept(() => {
+      store.replaceReducer(require("../redux/reducers").default);
+    });
+  }
+  return store;
+}
+
+function standaloneLogger() {
+  return (next) => (action) => next(action);
+}
+
+export default configureStore();
