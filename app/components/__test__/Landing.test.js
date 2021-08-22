ccd994b280f09439ebb6453362bae8ff22006e41
Srdjan Djenader <djenka76@gmail.com>
2021-08-22 12:49:48 -0500

    wording change

--- app/components/__test__/Landing.test.js
+++ app/components/__test__/Landing.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import Landing from "../screens/Landing";
 
-describe("Landing page should render with title", () => {
+describe("Landing screen should render with title", () => {
   it("should render title element", function() {
     const { getByTestId } = render(<Landing />);
     getByTestId("title");
